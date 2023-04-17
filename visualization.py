db8ebaddda4585402c9389e2d02f97b6f5e6eabe
VeraChristina <vera_gahlen@posteo.de>
2023-04-17 20:58:19 +0100

    fixed superposition metric

--- visualization.py
+++ visualization.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 from matplotlib.pyplot import matshow
 
-from training import ProjectAndRecover, load_saved_models, SPARSITIES
+from training import ProjectAndRecover, load_models_section2, SPARSITIES
 
 SMALL_MODELS_PATHNAME = "./model-weights/section2-small/"
 BIG_MODELS_PATHNAME = "./model-weights/section2-big/"
@@ -17,10 +17,10 @@ device = 'cpu'
 #%% load trained models
 if __name__ == "__main__":
     small_models = {}
-    load_saved_models(small_models)
+    load_models_section2(small_models)
 
     big_models = {}
-    load_saved_models(big_models, big = True)
+    load_models_section2(big_models, big = True)
 
 #%% Heat maps
 def plot_weights_and_bias(W, b):
@@ -39,7 +39,7 @@ def plot_weights_and_bias(W, b):
 
 
 #%% Visualization
-def superposition_metric(matrix: t.Tensor, new: bool = False) -> list[t.Tensor]:
+def superposition_metric(matrix: t.Tensor) -> list[t.Tensor]:
     """Compute metrics for representation and superposition for all column vectors
     
     W: input tensor of shape ( _ , num_features)
@@ -49,22 +49,19 @@ def superposition_metric(matrix: t.Tensor, new: bool = False) -> list[t.Tensor]:
     representation: tensor of shape (num_features) whose the j-th entry is the maximum norms of the column vectors W_j
     superposition: tensor of shape (num_features), 
     the j-th entry is the sum \sum_{i \neq j} (W_i * W_j)^2 over the squared inner product of W_j with all other column vectors W_i 
-    If new = True, the sum is normalized wrt the norm of W_i
+    If new = True, the sum is normalized wrt the norm of W_j
     """
     num_features = matrix.shape[1]
     representation = reduce(matrix*matrix, 'i j -> j', 'sum') ** .5
     
-    superposition = t.einsum('ij, il -> jl', matrix, matrix) ** 2
-    if new:
-        superposition = superposition / (repeat(representation, 'r -> n r', n=num_features) ** 2 + .0001)
-        
+    dot_products = t.einsum('ij, il -> jl', matrix, matrix) ** 2
     mask = t.ones((num_features, num_features)) - t.diag_embed(t.ones(num_features))
-    superposition = superposition * mask 
-    superposition = reduce(superposition, 'i j -> j', 'sum')
+    superposition = reduce(dot_products * mask, 'i j -> j', 'sum')
+    superposition = superposition / (representation ** 2 + 0.0001)
     return (representation, superposition)
 
         
-def visualize_superposition(W: t.Tensor, new: bool = False):
+def visualize_superposition(W: t.Tensor, sparsity: float, ax = None):
     """Plot histogram of superposition metric wrt all features
     W: input matrix of shape (hidden_dim, num_features)
     new: boolean that indicates which superposition metric to use
@@ -73,9 +70,10 @@ def visualize_superposition(W: t.Tensor, new: bool = False):
     color of bar corresponds to superposition metric of features
     """    
     num_features = W.shape[1]
-    representation, superposition = superposition_metric(W, new)
+    representation, superposition = superposition_metric(W)
     
-    fig, ax = plt.subplots()
+    if ax == None:
+        fig, ax = plt.subplots()
     features = range(num_features) 
     bars = representation.detach().numpy()
     color_values = superposition.detach().numpy()
@@ -88,8 +86,10 @@ def visualize_superposition(W: t.Tensor, new: bool = False):
     ax.set_ylabel('features')
     ax.set_box_aspect(2)
     ax.set_axis_off()
-
-    plt.show()
+    ax.set_title(f'sparsity = {sparsity}', fontsize=6)
+    
+    if ax == None:
+        plt.show()
 #%% test_visualize_superposition
 # matrix = t.diag_embed(t.ones(5))
 # print(matrix)
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     i = 1                                   # choose i <= 6
     model = small_models[SPARSITIES[i]] 
     plot_weights_and_bias(model.weights.data, model.bias.data)
-    visualize_superposition(model.weights)
+    visualize_superposition(model.weights, SPARSITIES[i])
 #%%
 if __name__ == "__main__":
     fig = plt.figure(figsize=(21.6, 3))
@@ -117,3 +117,10 @@ if __name__ == "__main__":
         ax.imshow(im, origin="upper", vmin= -1, vmax= 1, cmap=mlp.colormaps['PiYG'])
         #ax.set_label(f'W^t W and b for sparsity {sparsity}')
     plt.show()
+
+    fig = plt.figure(figsize=(8, 28))
+    for index in range(7):
+        ax = plt.subplot(171 + index)
+        W = small_models[SPARSITIES[index]].weights
+        visualize_superposition(W, SPARSITIES[index], ax)
+# %%
