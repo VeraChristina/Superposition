4a2d9fdc86e9899ba65180414eaaaa38f4a26e30
VeraChristina <vera_gahlen@posteo.de>
2023-04-18 21:55:30 +0100

    added utils file

---
+++ utils.py
@@ -0,0 +1,28 @@
+import torch as t
+
+
+def assert_shape_equal(actual: t.Tensor, expected: t.Tensor) -> None:
+    if actual.shape != expected.shape:
+        raise AssertionError(f"expected shape={expected.shape}, got {actual.shape}")
+
+
+def allclose(actual: t.Tensor, expected: t.Tensor, rtol=1e-4) -> None:
+    assert_shape_equal(actual, expected)
+    left = (actual - expected).abs()
+    right = rtol * expected.abs()
+    num_wrong = (left > right).sum().item()
+    if num_wrong > 0:
+        print(f"Test failed. Max absolute deviation: {left.max()}")
+        print(f"Actual:\n{actual}\nExpected:\n{expected}")
+        raise AssertionError(f"allclose failed with {num_wrong} / {left.nelement()} entries outside tolerance")
+    else:
+        print(f"Test passed with max absolute deviation of {left.max()}")
+
+
+def assert_all_equal(actual: t.Tensor, expected: t.Tensor) -> None:
+    """Assert that actual and expected are exactly equal (to floating point precision)."""
+    mask = actual == expected
+    if not mask.all().item():
+        bad = mask.nonzero()
+        msg = f"Did not match at {len(bad)} indexes: {bad[:10]}{'...' if len(bad) > 10 else ''}"
+        raise AssertionError(f"{msg}\nActual:\n{actual}\nExpected:\n{expected}")
